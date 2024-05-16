943745da44e9280526710081840c12516cf7fda6
ungjus <justinyong89@gmail.com>
2024-05-16 16:29:53 -0700

    feat: updated timeline to use chrono

--- src/lib/pages/timeline/index.tsx
+++ src/lib/pages/timeline/index.tsx
@@ -1,14 +1,15 @@
-// import { Play } from 'lucide-react';
+/* eslint-disable no-console */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable sonarjs/no-duplicate-string */
 import { useEffect, useState } from 'react';
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
+import { Chrono } from 'react-chrono';
+
+import { useTheme } from '@/lib/components/theme-provider';
 import 'react-vertical-timeline-component/style.min.css';
 
 interface Event {
-  date: string;
-  description: string;
+  title: string;
+  cardTitle: string;
 }
 
 interface EventsByYear {
@@ -16,7 +17,16 @@ interface EventsByYear {
 }
 
 const Timeline = () => {
+  const { theme } = useTheme();
   const [eventsByYear, setEventsByYear] = useState<EventsByYear>({});
+  const [chronoTheme, setChronoTheme] = useState({
+    primary: 'black',
+    secondary: theme === 'dark' ? 'rgb(23, 37, 84)' : 'rgb(253, 230, 138)',
+    cardBgColor: theme === 'dark' ? 'rgb(17, 24, 39)' : 'rgb(255, 255, 255)',
+    cardTitleColor: theme === 'dark' ? 'white' : 'black',
+    titleColor: theme === 'dark' ? 'white' : 'black',
+    titleColorActive: theme === 'dark' ? 'white' : 'black',
+  });
 
   const parseEvents = (text: string) => {
     const lines: string[] = text.split('\n');
@@ -29,8 +39,8 @@ const Timeline = () => {
         currentYear = trimmedLine;
         events[currentYear] = [];
       } else if (currentYear) {
-        const [date, description] = trimmedLine.split(' - ');
-        events[currentYear].push({ date, description });
+        const [title, cardTitle] = trimmedLine.split(' - ');
+        events[currentYear].push({ title, cardTitle });
       }
     });
 
@@ -52,32 +62,51 @@ const Timeline = () => {
     fetchEvents();
   }, []);
 
+  useEffect(() => {
+    console.log('theme changed', theme);
+
+    setChronoTheme((prevTheme) => ({
+      ...prevTheme,
+      secondary: theme === 'dark' ? 'rgb(23, 37, 84)' : 'rgb(253, 230, 138)',
+      cardBgColor: theme === 'dark' ? 'rgb(17, 24, 39)' : 'rgb(255, 255, 255)',
+      cardTitleColor: theme === 'dark' ? 'white' : 'black',
+      titleColor: theme === 'dark' ? 'white' : 'black',
+      titleColorActive: theme === 'dark' ? 'white' : 'black',
+    }));
+  }, [theme]);
+
+  console.log('Chrono theme:', chronoTheme);
+
   return (
     <div className="wrapper">
       {Object.entries(eventsByYear).map(([year, events]) => (
         <div key={year}>
           <h1 className="my-10 text-center text-3xl font-bold">{year}</h1>
-          <VerticalTimeline layout="2-columns">
-            {events.map((event) => (
-              <VerticalTimelineElement
-                key={event.date}
-                contentStyle={{ background: 'rgb(255, 255, 255)' }}
-                contentArrowStyle={{
-                  borderRight: '7px solid  rgb(255, 255, 255)',
-                }}
-                date={event.date}
-                dateClassName="date"
-                iconStyle={{
-                  background: 'rgb(0, 0, 0)',
-                  color: '#fff',
-                }}
-                // icon={<Play />}
-                position="right"
-              >
-                <h1>{event.description}</h1>
-              </VerticalTimelineElement>
-            ))}
-          </VerticalTimeline>
+          <Chrono
+            items={events}
+            mode="VERTICAL"
+            cardHeight="50"
+            disableToolbar="True"
+            theme={
+              theme === 'dark'
+                ? {
+                    primary: 'black',
+                    secondary: 'rgb(23, 37, 84)',
+                    cardBgColor: 'rgb(17, 24, 39)',
+                    cardTitleColor: 'white',
+                    titleColor: 'white',
+                    titleColorActive: 'white',
+                  }
+                : {
+                    primary: 'black',
+                    secondary: 'rgb(253, 230, 138)',
+                    cardBgColor: 'rgb(255, 255, 255)',
+                    cardTitleColor: 'black',
+                    titleColor: 'black',
+                    titleColorActive: 'black',
+                  }
+            }
+          />
         </div>
       ))}
     </div>
