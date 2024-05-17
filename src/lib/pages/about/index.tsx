2e71d45cc3da6d771f291d96f75bcc72607046c9
ungjus <justinyong89@gmail.com>
2024-05-16 23:18:38 -0700

    feat: init about me done!

--- src/lib/pages/about/index.tsx
+++ src/lib/pages/about/index.tsx
@@ -1,53 +1,185 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from '@/lib/components/ui/dialog';
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogTrigger,
+// } from '@/lib/components/ui/dialog';
 
-const images = [
-  'https://utfs.io/f/73b60dc3-9028-4f28-b879-f8c05bb99d37-xq66pv.JPEG',
-  'https://utfs.io/f/baea88db-c153-493d-b772-50bc13bdaebc-1xf5as.png',
-  'https://utfs.io/f/5ee852d1-88de-4afd-80d7-45bd90565679-debk18.JPEG',
-  'https://utfs.io/f/31c68226-5cb4-4f01-9d50-1eec26018182-1x9ebn.png',
-  'https://utfs.io/f/e2d4abeb-e808-4134-8d0d-d68344680948-1x9ezw.png',
-  'https://utfs.io/f/20580309-f263-46da-ae30-10d7398de833-1x9ezp.png',
-  'https://utfs.io/f/b2a7a809-7d0c-458a-8575-7b72643f2488-1x9w6w.png',
-  'https://utfs.io/f/127e3ea9-5589-421b-b3c1-e43f42cb849f-1x9e89.png',
-  'https://utfs.io/f/47b7e5c3-8636-41f6-a1fd-161558df8890-1xej08.png',
-  'https://utfs.io/f/de5d1ed5-8b26-4773-9656-6950082c1c2c-1xf29k.png',
-  'https://utfs.io/f/90cc3109-1888-44f9-b39a-55015afa5c6a-1xf0n0.png',
-  'https://utfs.io/f/1f447f85-19bf-49b6-8552-1c2622355edc-1xf26u.png',
-  'https://utfs.io/f/32bf1289-ef90-4060-8c9c-fe7ffcb8963c-1x9w7s.png',
-  'https://utfs.io/f/c7cfe050-9011-43bb-b0b9-7e47f82596cb-1xf2bc.png',
-];
-interface ImageCardProps {
-  src: string;
-}
+// const images = [
+//   'https://utfs.io/f/73b60dc3-9028-4f28-b879-f8c05bb99d37-xq66pv.JPEG',
+//   'https://utfs.io/f/baea88db-c153-493d-b772-50bc13bdaebc-1xf5as.png',
+//   'https://utfs.io/f/5ee852d1-88de-4afd-80d7-45bd90565679-debk18.JPEG',
+//   'https://utfs.io/f/31c68226-5cb4-4f01-9d50-1eec26018182-1x9ebn.png',
+//   'https://utfs.io/f/e2d4abeb-e808-4134-8d0d-d68344680948-1x9ezw.png',
+//   'https://utfs.io/f/20580309-f263-46da-ae30-10d7398de833-1x9ezp.png',
+//   'https://utfs.io/f/b2a7a809-7d0c-458a-8575-7b72643f2488-1x9w6w.png',
+//   'https://utfs.io/f/127e3ea9-5589-421b-b3c1-e43f42cb849f-1x9e89.png',
+//   'https://utfs.io/f/47b7e5c3-8636-41f6-a1fd-161558df8890-1xej08.png',
+//   'https://utfs.io/f/de5d1ed5-8b26-4773-9656-6950082c1c2c-1xf29k.png',
+//   'https://utfs.io/f/90cc3109-1888-44f9-b39a-55015afa5c6a-1xf0n0.png',
+//   'https://utfs.io/f/1f447f85-19bf-49b6-8552-1c2622355edc-1xf26u.png',
+//   'https://utfs.io/f/32bf1289-ef90-4060-8c9c-fe7ffcb8963c-1x9w7s.png',
+//   'https://utfs.io/f/c7cfe050-9011-43bb-b0b9-7e47f82596cb-1xf2bc.png',
+// ];
+// interface ImageCardProps {
+//   src: string;
+// }
 
-const ImageCard = ({ src }: ImageCardProps) => {
-  return (
-    <div className="overflow-hidden rounded-lg shadow-lg">
-      <img src={src} alt="Placeholder" className="h-auto w-full" />
-    </div>
-  );
-};
+// const ImageCard = ({ src }: ImageCardProps) => {
+//   return (
+//     <div className="overflow-hidden rounded-lg shadow-lg">
+//       <img src={src} alt="Placeholder" className="h-auto w-full" />
+//     </div>
+//   );
+// };
 
 const About = () => {
   return (
-    <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {images.map((image) => (
-          <Dialog>
-            <DialogTrigger>
-              <ImageCard key={image} src={image} />
-            </DialogTrigger>
-            <DialogContent>
-              <ImageCard key={image} src={image} />
-            </DialogContent>
-          </Dialog>
-        ))}
-      </div>
+    <div className="wrapper">
+      <h1 className="my-10 text-center text-4xl font-bold">About Us</h1>
+      <p className="text-center">
+        Today June 7, 2024 marks Justin and Kailey&apos;s the first anniversary!
+      </p>
+      <h2 className="my-5 text-center text-2xl font-bold">How We Met</h2>
+      <p>
+        Justin and Kailey met as teaching assistants for CSE 121 in fall 2023.
+        It was an exciting time because it was the first time the course was
+        being taught. Kailey was previously taught CSE 162 teaching assistant
+        and Justin previously taught CSE 142, the predecessor to CSE 121.
+        Because of this, Justin had some experience with the course structure
+        and was a lead for that quarter. He even gave a self introduction during
+        the first Staff Meeting (though kailey doesn&apos;t remember this).
+        However, their first interactions with each other were in the copy room
+        of CSE2. Kailey was in the room with other TA&apos;s and the copy
+        machine wasn&apos;t working. That&apos;s when Justin opened the door
+        into the room.
+      </p>
+      <br />
+      <p>Kailey&apos;s first impression of Justin: Tan and small head.</p>
+      <p>
+        Justin&apos;s first impression of Kailey: Wearing mask and large
+        forehead
+      </p>
+
+      <br />
+
+      <p>
+        After resolving the prining issue and getting their section copies, they
+        began walking home. Justin lived in Savannah appartments and Kailey
+        lived in Nordheim at the time. However, for some reason Kailey was
+        walking up the Ave as well. This was when Justin and Kailey first
+        started talking with each other.
+      </p>
+      <h2 className="my-5 text-center text-2xl font-bold">
+        Getting Closer to Each Other
+      </h2>
+      <p>
+        After their first fated meeting, they didn&apos;t talk much to each
+        other afterwards. They would occasionally See each other during lectures
+        and walk home together after meetings but nothing beyond that. During
+        one of they lectures, however, Kailey gifted Justin a Blue Lock sticker
+        and Pokemon charm which started Justin&apos;s long list of indebtments
+        to Kailey. As the end of the quarter neared, Justin messaged Kailey
+        though Instagram to remind her to apply to be a lead TA. This started
+        Justin and Kailey&apos;s friendship.
+      </p>
+      <br />
+
+      <p>
+        Justin started playing Pikmin bloom with Kailey and quickly became
+        addicted. As they grew closer Justin asked for Kailey&apos;s phone
+        number since she rarely checked Instagram. On Feburary 15, 2023, Justin
+        and Kailey got dinner at Mee Sum and offically became besties there.
+        They grew closer and did things like watching anime, playing games,
+        watching application videos, working on assingments, origami and having
+        late night discord calls. They got their first meal together at
+        Araya&apos;s Place for lunch.
+      </p>
+
+      <h2 className="my-5 text-center text-2xl font-bold">The Confession</h2>
+
+      <p>
+        On May 24, 2023, Justin made his decision to express his feelings
+        towards Kailey. That day, Justin went over to Nordheim to play ping pong
+        with Kailey before their leads meeting. It was after the leads meeting
+        when Justin and Kailey stayed afterwards in the room where Justin
+        prepard to stay it. Kailey came back from bathroom, sat down, closed her
+        computer, and said &quot;Okay I&apos;m ready to leave&quot;. That is
+        when Justin said &quot;Wait I have something to tell you...&quot;.
+        However, after the confession, Kailey had conflicted feelings and she
+        needed time to think about it.
+      </p>
+      <br />
+      <p>
+        What comes next was awkward since Justin and Kailey still needed to go
+        print section copies. They both stood around the copy machine in silence
+        as the machine slowly printed the copies and awkwardly walked home.
+        However, the awkwardness didn&apos;t last long the next day they greeted
+        each other between sections as Justin was preparing to leave the room.
+        They played badminton and tennis a couple days later and watched more
+        application videos. They quickly went back to being besties again.
+      </p>
+
+      <h2 className="my-5 text-center text-2xl font-bold">The Response</h2>
+
+      <p>
+        On June 6, 2023, Justin and Kailey hosted a review session in
+        preparation for the Spring CSE 121 final. After the review session,
+        Justin suggsted playing some vollyball and encouraging our good friend
+        Jaden Tan before his interview to become as CSE 12X TA. Kaiely took this
+        as a perfect opportunity to say her response. So on June 7, 2023, Justin
+        and Kailey briefly played some vollyball before realizing how bad they
+        were and how much their forearms hurt. They got dinner at Miss
+        Mike&apos;s Bento & Noodles before making their way over the the CSE1
+        building to meet their favorite lil person. Afterwards, they began
+        walking home. Justin didn&apos;t thnk much about it at the time but
+        Kailey was frantically trying to find the right time to tell Justin
+        something. At the corner of the top of the hill where Justin and Kailey
+        usually say bye to each other, Justin says &quot;Okay bye bye&quot; and
+        Kailey urgently says &quot;Wait! My response&quot; The rest is history.
+      </p>
+
+      <h2 className="my-5 text-center text-2xl font-bold">
+        Future Aspirations
+      </h2>
+
+      <p>
+        Justin and Kailey grew a lot this past year. As a couple, they grew
+        closer. They found new hobbies, dealt with long distance, had their
+        first micro confict, met each other&apos;s parents and families. They
+        also grew professionally as they both started working. However, they
+        still make time for each other every week.
+      </p>
+      <br />
+      <p>
+        Going into the next year, Justin and Kailey are looking forward to
+        growing more together. Justin will probably mett Kailey&apos;s mom for
+        the first time in person this year as well as getting food with the Tou
+        family. Justin and Kailey&apos;s families might meet each other. Justin
+        and Kailey also hope to go on a trip to Japan together next year. But
+        regardless of what they do, they are looking forward to more adventures
+        and creating more memories together.
+      </p>
+
+      <img
+        src="/images/redbean.png"
+        alt="Us as red beans"
+        className="item-center mx-auto flex size-2/3 justify-center"
+      />
     </div>
+    // <div className="container mx-auto px-4">
+    //   <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    //     {images.map((image) => (
+    //       <Dialog>
+    //         <DialogTrigger>
+    //           <ImageCard key={image} src={image} />
+    //         </DialogTrigger>
+    //         <DialogContent>
+    //           <ImageCard key={image} src={image} />
+    //         </DialogContent>
+    //       </Dialog>
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 export default About;
