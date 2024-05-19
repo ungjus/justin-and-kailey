1c81d9cd12ed96dac85a95f11739c665db054b0f
ungjus <justinyong89@gmail.com>
2024-05-18 19:28:08 -0700

    style: added more text to banner

--- src/lib/pages/home/components/Banner.tsx
+++ src/lib/pages/home/components/Banner.tsx
@@ -13,13 +13,20 @@ const Banner = () => {
   }
 
   return (
-    <div className="wrapper flex w-full items-center justify-between rounded-md bg-yellow-300 px-4 py-2 text-black shadow-md dark:bg-blue-500 dark:text-white">
-      <span className="ml-9 flex-grow text-center font-bold">
-        Surprise! Happy Anniversary ❤️
-      </span>
+    <div className="relative w-full rounded-md bg-yellow-300 px-4 py-2 text-black shadow-md dark:bg-blue-500 dark:text-white">
+      <div className="text-center">
+        <span className="mt-5 block font-bold">
+          Surprise! Happy Anniversary ❤️
+        </span>
+        <br />
+        <span className="block">
+          I wanted to make you something special so decided to make a website!
+        </span>
+        <span className="block">Take a Look around and enjoy :)</span>
+      </div>
       <X
         onClick={handleClose}
-        className="ml-auto text-gray-600 dark:text-white"
+        className="absolute right-0 top-0 mr-2 mt-2 text-black dark:text-white"
       />
     </div>
   );
