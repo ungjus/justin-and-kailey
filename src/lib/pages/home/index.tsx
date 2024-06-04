a47920c66ccc4b86348ac3a0fe894f746c0be021
ungjus <justinyong89@gmail.com>
2024-06-03 23:22:59 -0700

    feat: added home pic

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -8,6 +8,11 @@ const Home = () => {
       <div className="flex justify-center">
         <HomeCarousel />
       </div>
+      <img
+        src="https://utfs.io/f/2018f8d5-2505-4942-89eb-5c9da1294985-819qza.jpg"
+        alt="Kailey and Justin"
+        className="item-center mx-auto flex size-1/2 justify-center rounded-lg"
+      />
     </div>
   );
 };
